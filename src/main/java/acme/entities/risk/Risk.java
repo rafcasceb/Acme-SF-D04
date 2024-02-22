db18ba224b92638a0e2cc869dd32cff732d33172
Rafael Castillo Cebolla <rafaelcastillocebolla@gmail.com>
2024-02-22 10:18:36 +0100

    feat(entities): Risk entity #85
    Entities package has been created.
    Assumed if "optional" not specified, it's not null.
    Assumed probability is a percentage.

---
+++ src/main/java/acme/entities/risk/Risk.java
@@ -0,0 +1,62 @@
+
+package acme.entities.risk;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+
+public class Risk extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------------------------
+
+	@Column(unique = true)
+	@Pattern(regexp = "R-[0-9]{3}")
+	@NotBlank
+	private String				reference;
+
+	@Temporal(TemporalType.DATE)
+	@Past
+	@NotNull
+	private Date				identificationDate;
+
+	@Min(0)
+	@NotNull
+	private double				impact;
+
+	@Min(0)
+	@Max(100)
+	@NotNull
+	private double				probability;
+
+	@Length(max = 100)
+	@NotBlank
+	private String				description;
+
+	@URL
+	private String				link;
+
+
+	@Transient
+	public Double getValue() {
+		return this.impact * this.probability;
+	}
+
+}
