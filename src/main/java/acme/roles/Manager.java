e89735cc4f1fb956409ad90757376dd77ec4771d
Rafael Castillo Cebolla <rafaelcastillocebolla@gmail.com>
2024-02-23 08:32:34 +0100

    feat(Roles): Manager role created #117
    Created Manager role.
    Updated individual requirements document.

--- src/main/java/acme/roles/Manager.java
+++ src/main/java/acme/roles/Manager.java
@@ -1,8 +1,40 @@
 
 package acme.roles;
 
-import acme.client.data.AbstractRole;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Manager extends AbstractRole {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Length(max = 75)
+	@NotBlank
+	private String				degree;
+
+	@Length(max = 100)
+	@NotBlank
+	private String				overview;
+
+	@Length(max = 100)
+	@NotBlank
+	private String				certifications;
+
+	@URL
+	private String				link;
 
 }
