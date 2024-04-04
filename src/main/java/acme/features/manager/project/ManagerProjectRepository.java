10e4ef46675195d714cafac41ec63e532ff17115
Rafael Castillo Cebolla <rafaelcastillocebolla@gmail.com>
2024-04-04 12:30:57 +0200

    feat(Manager): Show project #268

--- src/main/java/acme/features/manager/project/ManagerProjectRepository.java
+++ src/main/java/acme/features/manager/project/ManagerProjectRepository.java
@@ -12,6 +12,9 @@ import acme.entities.projects.Project;
 @Repository
 public interface ManagerProjectRepository extends AbstractRepository {
 
+	@Query("select p from Project p where p.id = :id")
+	Project findOneProjectById(int id);
+
 	@Query("select p from Project p where p.manager.id = :managerId")
 	Collection<Project> findManyProjectsByManagerId(int managerId);
 
---
+++ src/main/java/acme/features/manager/project/ManagerProjectShowService.java
@@ -0,0 +1,50 @@
+
+package acme.features.manager.project;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.data.models.Dataset;
+import acme.client.services.AbstractService;
+import acme.entities.projects.Project;
+import acme.roles.Manager;
+
+@Service
+public class ManagerProjectShowService extends AbstractService<Manager, Project> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private ManagerProjectRepository repository;
+
+	// AbstractService interface ----------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Project object;
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		object = this.repository.findOneProjectById(id);
+
+		super.getBuffer().addData(object);
+	}
+
+	@Override
+	public void unbind(final Project object) {
+		assert object != null;
+
+		Dataset dataset;
+
+		dataset = super.unbind(object, "published", "code", "title", "abstractDescription", "fatalErrorPresent", "estimatedCostInHours", "link");
+
+		super.getResponse().addData(dataset);
+	}
+
+}
