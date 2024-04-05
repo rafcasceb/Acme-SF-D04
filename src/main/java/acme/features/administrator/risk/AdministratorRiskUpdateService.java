ed159cf16cbbe8c2f3996f622d74fb7298036d24
Rafael Castillo Cebolla <rafaelcastillocebolla@gmail.com>
2024-04-05 10:43:44 +0200

    feat(Admin): List and show Risks #296
    Initialized create, update and delete services as well.

---
+++ src/main/java/acme/features/administrator/risk/AdministratorRiskUpdateService.java
@@ -0,0 +1,21 @@
+
+package acme.features.administrator.risk;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.data.accounts.Administrator;
+import acme.client.services.AbstractService;
+import acme.entities.risks.Risk;
+
+@Service
+public class AdministratorRiskUpdateService extends AbstractService<Administrator, Risk> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AdministratorRiskRepository repository;
+
+	// AbstractService interface ----------------------------------------------
+
+}
