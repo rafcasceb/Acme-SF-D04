5a43cbc99b5ced59eb9bfccc174b58a72c7c411f
Rafael Castillo Cebolla <rafaelcastillocebolla@gmail.com>
2024-04-05 12:13:23 +0200

    feat(Admin): Read, Update and Delete Risk #296

--- src/main/java/acme/features/administrator/risk/AdministratorRiskUpdateService.java
+++ src/main/java/acme/features/administrator/risk/AdministratorRiskUpdateService.java
@@ -1,10 +1,14 @@
 
 package acme.features.administrator.risk;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import acme.client.data.accounts.Administrator;
+import acme.client.data.models.Dataset;
+import acme.client.helpers.MomentHelper;
 import acme.client.services.AbstractService;
 import acme.entities.risks.Risk;
 
@@ -17,5 +21,73 @@ public class AdministratorRiskUpdateService extends AbstractService<Administrato
 	private AdministratorRiskRepository repository;
 
 	// AbstractService interface ----------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		boolean status;
+		int masterId;
+		Risk risk;
+
+		masterId = super.getRequest().getData("id", int.class);
+		risk = this.repository.findOneRiskById(masterId);
+		status = risk != null && super.getRequest().getPrincipal().hasRole(Administrator.class);
+
+		super.getResponse().setAuthorised(status);
+	}
+
+	@Override
+	public void load() {
+		Risk object;
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		object = this.repository.findOneRiskById(id);
+
+		super.getBuffer().addData(object);
+	}
+
+	@Override
+	public void bind(final Risk object) {
+		assert object != null;
+
+		super.bind(object, "reference", "identificationDate", "impact", "probability", "description", "link");
+	}
+
+	@Override
+	public void validate(final Risk object) {
+		assert object != null;
+
+		if (!super.getBuffer().getErrors().hasErrors("identificationDate")) {
+			Date minimumDate;
+
+			minimumDate = MomentHelper.parse("01/01/2000", "dd/MM/yyyy");
+			super.state(MomentHelper.isAfter(object.getIdentificationDate(), minimumDate), "probability", "manager.project.form.error.identification-date");
+		}
+
+		if (!super.getBuffer().getErrors().hasErrors("impact"))
+			super.state(object.getImpact() > 0.0 && object.getImpact() < 1.0, "impact", "manager.project.form.error.impact");
+
+		if (!super.getBuffer().getErrors().hasErrors("probability"))
+			super.state(object.getProbability() > 0.0 && object.getProbability() < 1.0, "probability", "manager.project.form.error.probability");
+	}
+
+	@Override
+	public void perform(final Risk object) {
+		assert object != null;
+
+		this.repository.save(object);
+	}
+
+	@Override
+	public void unbind(final Risk object) {
+		assert object != null;
+
+		Dataset dataset;
+
+		dataset = super.unbind(object, "reference", "identificationDate", "impact", "probability", "description", "link");
+
+		super.getResponse().addData(dataset);
+	}
 
 }
--- src/main/webapp/WEB-INF/views/administrator/risk/form.jsp
+++ src/main/webapp/WEB-INF/views/administrator/risk/form.jsp
@@ -17,10 +17,20 @@
 
 <acme:form>
 	<acme:input-textbox code="administrator.risk.form.label.reference" path="reference"/>
-	<acme:input-textbox code="administrator.risk.form.label.identificationDate" path="identificationDate"/>
-	<acme:input-textbox code="administrator.risk.form.label.impact" path="impact"/>
-	<acme:input-textbox code="administrator.risk.form.label.probability" path="probability"/>
-	<acme:input-textbox code="administrator.risk.form.label.value" path="value"/>
+	<acme:input-moment code="administrator.risk.form.label.identificationDate" path="identificationDate"/>
+	<acme:input-double code="administrator.risk.form.label.impact" path="impact"/>
+	<acme:input-double code="administrator.risk.form.label.probability" path="probability"/>
 	<acme:input-textbox code="administrator.risk.form.label.description" path="description"/>
-	<acme:input-textbox code="administrator.risk.form.label.link" path="link"/>
+	<acme:input-url code="administrator.risk.form.label.link" path="link"/>
+	
+	<jstl:choose>
+		<jstl:when test="${acme:anyOf(_command, 'show|update|delete')}">
+			<acme:submit code="administrator.risk.form.button.update" action="/administrator/risk/update"/>
+			<acme:submit code="administrator.risk.form.button.delete" action="/administrator/risk/delete"/>
+		</jstl:when>
+		<jstl:when test="${_command == 'create'}">
+			<acme:submit code="administrator.risk.form.button.create" action="/administrator/risk/create"/>
+		</jstl:when>	
+	</jstl:choose>
+		
 </acme:form>
