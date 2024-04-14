093f9279b64f36488ed9f841140ed6ccc50c610c
Luis Mellado Diaz <luimeldia@alum.us.es>
2024-04-14 18:00:41 +0200

    feat(Banner): Operations by Any Display Period #301

--- src/main/java/acme/components/AnyBannerRepository.java
+++ src/main/java/acme/components/AnyBannerRepository.java
@@ -12,14 +12,17 @@
 
 package acme.components;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import acme.client.helpers.MomentHelper;
 import acme.client.helpers.RandomHelper;
 import acme.client.repositories.AbstractRepository;
 import acme.entities.banners.Banner;
@@ -27,19 +30,20 @@ import acme.entities.banners.Banner;
 @Repository
 public interface AnyBannerRepository extends AbstractRepository {
 
-	@Query("select count(b) from Banner b")
-	int countBanner();
+	@Query("select count(b) from Banner b where b.displayStartMoment < :currentMoment and b.displayEndMoment > :currentMoment")
+	int countBanner(@Param("currentMoment") Date currentMoment);
 
-	@Query("select b from Banner b")
-	List<Banner> findManyBanners(PageRequest pageRequest);
+	@Query("SELECT b FROM Banner b WHERE b.displayStartMoment < :currentMoment AND b.displayEndMoment > :currentMoment")
+	List<Banner> findManyBanners(@Param("currentMoment") Date currentMoment, PageRequest pageRequest);
 
 	default Banner findRandomBanner() {
 		Banner result;
 		int count, index;
 		PageRequest page;
 		List<Banner> list;
+		Date currentMoment = MomentHelper.getCurrentMoment();
 
-		count = this.countBanner();
+		count = this.countBanner(currentMoment);
 
 		if (count == 0)
 			result = null;
@@ -47,7 +51,7 @@ public interface AnyBannerRepository extends AbstractRepository {
 			index = RandomHelper.nextInt(0, count);
 
 			page = PageRequest.of(index, 1, Sort.by(Direction.ASC, "id"));
-			list = this.findManyBanners(page);
+			list = this.findManyBanners(currentMoment, page);
 			result = list.isEmpty() ? null : list.get(0);
 		}
 		return result;
